91486976571159ba3a552b331b2674fdb96f1330
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-08-16 20:14:02 +0300

    Initial Commit

---
+++ BuildTinder/BuildTinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTinderApp.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BuildTinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
