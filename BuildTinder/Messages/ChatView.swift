a101f3a45331ee523efb66afde9ad5690d2fada5
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-08-29 22:49:15 +0300

    style: design improvement on chat view

--- BuildTinder/Messages/ChatView.swift
+++ BuildTinder/Messages/ChatView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ChatView: View {
     @ObservedObject var chatMng: ChatManager
+    @State private var typingMessage: String = ""
+    
     private var person: Person
     
     init(person: Person){
@@ -16,14 +18,54 @@ struct ChatView: View {
         self.chatMng = ChatManager(person: person)
     }
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false, content: {
-            LazyVStack{
-                ForEach(chatMng.messages.indices, id: \.self) { index in
-                    let msg = chatMng.messages[index]
-                    MessageView(message: msg)
+        ZStack (alignment: .top){
+            VStack {
+                Spacer()
+                    .frame(height: 60)
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    LazyVStack{
+                        ForEach(chatMng.messages.indices, id: \.self) { index in
+                            let msg = chatMng.messages[index]
+                            MessageView(message: msg)
+                        }
+                    }
+                })
+                ZStack(alignment: .trailing) {
+                    Color.texttfieldBG
+                    TextField("Type a message", text: $typingMessage)
+                        .foregroundColor(Color.textPrimary)
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .frame(height: 45)
+                        .padding(.horizontal)
+                    Button(action: { sendMessage() }, label: {
+                        Text("Send")
+                    })
+                    .padding(.horizontal)
+                    .foregroundColor(typingMessage.isEmpty ? Color.textPrimary : Color.blue )
                 }
+                .frame(height: 40)
+                .cornerRadius(20)
+                .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+                    )
+                .padding(.horizontal)
+                .padding(.bottom)
+                
             }
-        })
+            ChatViewHeader(
+                name: person.name,
+                imageURL: person.imagesURLS.first) {
+                    // Video Action
+                }
+        }
+        .navigationTitle("")
+        .navigationBarHidden(true)
+    }
+    
+    func sendMessage() {
+        chatMng.sendMessage(Message(content: typingMessage))
+        typingMessage = ""
     }
 }
 
