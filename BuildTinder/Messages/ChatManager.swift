ac06b63c2cf27cbe79db403808748bfbcbb1baf1
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-08-30 01:06:26 +0300

    build: Implementing publishers to listen for keyboard notification

--- BuildTinder/Messages/ChatManager.swift
+++ BuildTinder/Messages/ChatManager.swift
@@ -6,15 +6,21 @@
 //
 
 import Foundation
+import Combine
+import UIKit
 
 class ChatManager: ObservableObject {
     @Published var messages:[Message] = []
+    @Published var keyboardIsShowing: Bool  = false
     
+    
+    var cancellable: AnyCancellable? = nil  
     private var person: Person
     
     init(person: Person){
         self.person = person
         loadMessages()
+        setupPublishers()
     }
     
     
@@ -24,7 +30,26 @@ class ChatManager: ObservableObject {
         // if networking failure show error with some retry options
     }
     
+    private let keyboardWillShow = NotificationCenter.default
+        .publisher(for: UIResponder.keyboardWillShowNotification)
+        .map({_ in true })
+    
+    private let keybardWillHide = NotificationCenter.default
+        .publisher(for: UIResponder.keyboardWillHideNotification)
+        .map({_ in false })
+    
+    private func setupPublishers() {
+        cancellable = Publishers.Merge(keyboardWillShow, keybardWillHide)
+            .subscribe(on: DispatchQueue.main)
+            .assign(to: \.keyboardIsShowing, on: self)
+        
+    }
+    
     private func loadMessages(){
           messages = [Message.exampleSent, Message.exampleRecieved]
+    }
+    
+    deinit {
+        cancellable?.cancel()
     }
 }
