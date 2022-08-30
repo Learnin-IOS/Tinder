cc1b2d5072a558ff7de11e88f59107b76d691047
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-08-30 22:00:43 +0300

    build: MessageList View Model

---
+++ BuildTinder/Messages/MessageListVM.swift
@@ -0,0 +1,23 @@
+//
+//  MessageListVM.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 30/08/2022.
+//
+
+import Foundation
+
+
+class MessageListVM: ObservableObject{
+    
+    @Published var messagePreviews: [MessagePreview] = []
+    
+    init() {
+        loadPreviews()
+    }
+    
+    func loadPreviews(){
+        // Handle networking to load messagePreviews from a Server
+        self.messagePreviews = [MessagePreview.example]
+    }
+}
