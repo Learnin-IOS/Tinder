a6dec42d3393954d7c19f395a51533652e4e18f1
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-08-30 21:46:22 +0300

    build: MessageListView

---
+++ BuildTinder/Messages/MessageListView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageListView.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 30/08/2022.
+//
+
+import SwiftUI
+
+struct MessageListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageListView()
+    }
+}
