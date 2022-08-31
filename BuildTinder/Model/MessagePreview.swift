d812310d2a3d49c69ba513abdec43ad00a8a26f5
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-08-31 04:15:45 +0300

    refactor: adding more Mock Up message previews

--- BuildTinder/Model/MessagePreview.swift
+++ BuildTinder/Model/MessagePreview.swift
@@ -8,11 +8,17 @@
 import Foundation
 
 
-struct MessagePreview {
+struct MessagePreview: Hashable {
     var person: Person
     var lastMessage: String
 }
 
 extension MessagePreview {
     static let example = MessagePreview(person: Person.example, lastMessage: "Hello there. How are you doing today? How is the weather were you live?")
+    
+    static let examples: [MessagePreview] = [
+        MessagePreview(person: Person.example, lastMessage: "Hello there. How are you doing today? How is the weather were you live?"),
+        MessagePreview(person: Person.example2, lastMessage: "Hi there.")
+        
+    ]
 }
