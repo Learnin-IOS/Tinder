cc9d3dcc8057568779885c11d518eb0c255b1981
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-08-31 19:22:41 +0300

    build: Search Optimisation

--- BuildTinder/Model/Person.swift
+++ BuildTinder/Model/Person.swift
@@ -11,9 +11,19 @@ import Foundation
 struct Person: Hashable {
     var name: String
     var imagesURLS: [URL]
+    var bio: String
 }
 
 extension Person {
-    static let example = Person(name: "Alex", imagesURLS: [URL(string: "https://picsum.photos/400/300")!])
-    static let example2 = Person(name: "Patrick", imagesURLS: [URL(string: "https://picsum.photos/400/301")!])
+    static let example = Person(
+        name: "Alex",
+        imagesURLS: [URL(string: "https://picsum.photos/400/300")!],
+        bio: "Gaming is my life"
+    )
+    
+    static let example2 = Person(
+        name: "Patrick",
+        imagesURLS: [URL(string: "https://picsum.photos/400/301")!],
+        bio: "I really love cats"
+    )
 }
