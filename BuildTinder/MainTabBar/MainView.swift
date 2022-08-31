9a7795e084818073236be7a6af0443812b0c2f06
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-08-31 19:23:18 +0300

    refactor: Replacing dummy text with actual MessageListView

--- BuildTinder/MainTabBar/MainView.swift
+++ BuildTinder/MainTabBar/MainView.swift
@@ -23,7 +23,7 @@ struct MainView: View {
             let view = Text("Star")
             return AnyView(view)
         case .message:
-            let view = Text("Message")
+            let view = MessageListView()
             return AnyView(view)
         case .profile:
             let view = ProfileView()
@@ -31,37 +31,40 @@ struct MainView: View {
         }
     }
     var body: some View {
-        ZStack{
-            Color(.systemGray6)
-                .opacity(0.35)
-                .edgesIgnoringSafeArea(.vertical)
-            VStack{
-                HStack {
-                    Spacer()
-                    
-                    TabBarButtonView(type: .fire)
-                    
-                    Spacer()
-                    
-                    TabBarButtonView(type: .star)
-                    
-                    Spacer()
-                    
-                    TabBarButtonView(type: .message)
-                    
-                    Spacer()
-                    
-                    TabBarButtonView(type: .profile)
+        NavigationView  {
+            ZStack{
+                Color(.systemGray6)
+                    .opacity(0.35)
+                    .edgesIgnoringSafeArea(.vertical)
+                VStack{
+                    HStack {
+                        Spacer()
+                        
+                        TabBarButtonView(type: .fire)
+                        
+                        Spacer()
+                        
+                        TabBarButtonView(type: .star)
+                        
+                        Spacer()
+                        
+                        TabBarButtonView(type: .message)
+                        
+                        Spacer()
+                        
+                        TabBarButtonView(type: .profile)
+                        
+                        Spacer()
+                    }
+                    .frame( height: 100)
+                    .padding(.top, 30)
+                    correctViewForState()
                     
                     Spacer()
                 }
-                .frame( height: 100)
-                .padding(.top, 30)
-                correctViewForState()
-                
-                Spacer()
+                .edgesIgnoringSafeArea(.vertical)
             }
-            .edgesIgnoringSafeArea(.vertical)
+            .modifier(HideNavigationView())
         }
         
     }
