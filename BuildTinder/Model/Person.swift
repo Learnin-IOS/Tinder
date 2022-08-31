3066b113ea9c8dfb77ca14a8fb0c96f42268d422
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-08-31 04:14:59 +0300

    refactor: creating more Mock Up users

--- BuildTinder/Model/Person.swift
+++ BuildTinder/Model/Person.swift
@@ -15,4 +15,5 @@ struct Person: Hashable {
 
 extension Person {
     static let example = Person(name: "Alex", imagesURLS: [URL(string: "https://picsum.photos/400/300")!])
+    static let example2 = Person(name: "Patrick", imagesURLS: [URL(string: "https://picsum.photos/400/301")!])
 }
