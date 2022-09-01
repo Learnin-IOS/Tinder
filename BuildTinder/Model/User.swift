98571d863cc57c86c5cf929fdc1d3c65d056cee3
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-01 15:21:39 +0300

    build: A struct to help user management

---
+++ BuildTinder/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 01/09/2022.
+//
+
+import Foundation
+
+struct User {
+    var name: String
+    
+    var goldSubscriber: Bool = false 
+}
