a5da130f3d227bb07104cfe25f7fb03ac970c970
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-01 15:22:30 +0300

    build: class to manage users using the User struct

---
+++ BuildTinder/UserManager.swift
@@ -0,0 +1,12 @@
+//
+//  UserManager.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 01/09/2022.
+//
+
+import Foundation
+
+class UserManager: ObservableObject {
+    @Published var currentUser: User = User(nam: <#T##String#>)
+}
