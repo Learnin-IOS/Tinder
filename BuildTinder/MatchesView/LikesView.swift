022e9c240cf34c3bdc6560ee0746fc160a69db3b
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-01 15:56:07 +0300

    build: LikesView

---
+++ BuildTinder/MatchesView/LikesView.swift
@@ -0,0 +1,20 @@
+//
+//  LikesView.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 01/09/2022.
+//
+
+import SwiftUI
+
+struct LikesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LikesView_Previews: PreviewProvider {
+    static var previews: some View {
+        LikesView()
+    }
+}
