9589e0de06a2bbedaba2a927c2f45652dca38b3e
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-07 00:56:30 +0300

    style: Designing person square with ability to blur for non- gold Subscribers

--- BuildTinder/MatchesView/PersonSquare.swift
+++ BuildTinder/MatchesView/PersonSquare.swift
@@ -6,15 +6,36 @@
 //
 
 import SwiftUI
+import KingfisherSwiftUI
 
 struct PersonSquare: View {
+    var person: Person
+    var blur: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottomLeading) {
+            KFImage(person.imagesURLS.first)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 240)
+                .if(blur) {
+                    $0.blur(radius: 25)
+                }
+            
+            Text("\(person.name), \(person.age)")
+                .padding()
+                .foregroundColor(.white)
+                .font(.system(size: 18, weight: .semibold))
+                .if(blur) {
+                    $0.redacted(reason: .placeholder)
+                }
+        }
+        .cornerRadius(16)
     }
 }
 
 struct PersonSquare_Previews: PreviewProvider {
     static var previews: some View {
-        PersonSquare()
+        PersonSquare(person: Person.example, blur: false)
     }
 }
