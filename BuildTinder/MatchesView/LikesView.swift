c66a74e783e649755d4fdd50d575095d05a1e4de
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-06 22:49:46 +0300

    feat: Likes View

--- BuildTinder/MatchesView/LikesView.swift
+++ BuildTinder/MatchesView/LikesView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct LikesView: View {
+    @EnvironmentObject var userMng: UserManager
+    
+    private var user: User {
+        return userMng.currentUser
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false, content: {
+            
+            if !user.goldSubscriber {
+            Text("Upgrade to Gold to see the people that already liked you.")
+                .multilineTextAlignment(.center)
+                .lineLimit(2)
+                .foregroundColor(.textTitle)
+                .font(.system(size: 16, weight: .medium))
+                .padding(.horizontal, 40)
+                .padding(.vertical, 24)
+            }
+            
+            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())],
+                      alignment: .center,
+                      spacing: nil,
+                      pinnedViews: [],
+                      content: {
+                Text("Placeholder")
+                Text("Placeholder")
+                
+            })
+        })
     }
 }
 
 struct LikesView_Previews: PreviewProvider {
     static var previews: some View {
         LikesView()
+            .environmentObject(UserManager())
     }
 }
