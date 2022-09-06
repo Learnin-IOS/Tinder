029d2e5f5c25ace18aa993116984677f674eb2fd
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-07 01:59:17 +0300

    style: Increasing support for many users

--- BuildTinder/MatchesView/PersonSquare.swift
+++ BuildTinder/MatchesView/PersonSquare.swift
@@ -13,29 +13,32 @@ struct PersonSquare: View {
     var blur: Bool
     
     var body: some View {
-        ZStack(alignment: .bottomLeading) {
-            KFImage(person.imagesURLS.first)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(height: 240)
-                .if(blur) {
-                    $0.blur(radius: 25)
-                }
-            
-            Text("\(person.name), \(person.age)")
-                .padding()
-                .foregroundColor(.white)
-                .font(.system(size: 18, weight: .semibold))
-                .if(blur) {
-                    $0.redacted(reason: .placeholder)
-                }
+        GeometryReader { geo in
+            ZStack(alignment: .bottomLeading) {
+                KFImage(person.imagesURLS.first)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geo.size.width, height: 240)
+                    .if(blur) {
+                        $0.blur(radius: 25)
+                    }
+                
+                Text("\(person.name), \(person.age)")
+                    .padding()
+                    .foregroundColor(.white)
+                    .font(.system(size: 18, weight: .semibold))
+                    .if(blur) {
+                        $0.redacted(reason: .placeholder)
+                    }
+            }
+            .cornerRadius(16)
         }
-        .cornerRadius(16)
     }
 }
 
 struct PersonSquare_Previews: PreviewProvider {
     static var previews: some View {
         PersonSquare(person: Person.example, blur: false)
+            .frame(width: 125)
     }
 }
