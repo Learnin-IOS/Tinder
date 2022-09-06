abb5af3b083acc2dbadff818c39cbc68c97bba05
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-07 00:12:45 +0300

    style: Designing Squares that will contain in the Grid

---
+++ BuildTinder/MatchesView/PersonSquare.swift
@@ -0,0 +1,20 @@
+//
+//  PersonSquare.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 07/09/2022.
+//
+
+import SwiftUI
+
+struct PersonSquare: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonSquare_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonSquare()
+    }
+}
