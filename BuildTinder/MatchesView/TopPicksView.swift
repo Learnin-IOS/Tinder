911bad7af4a76761f9d41737f24c5df5e621c301
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-08 20:04:16 +0300

    build: Adding top picks view

---
+++ BuildTinder/MatchesView/TopPicksView.swift
@@ -0,0 +1,20 @@
+//
+//  TopPicksView.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 08/09/2022.
+//
+
+import SwiftUI
+
+struct TopPicksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopPicksView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopPicksView()
+    }
+}
