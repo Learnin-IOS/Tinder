e6d7e73d4038a4aefd59dc44b039c0314d1d21e1
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-08 21:00:21 +0300

    feat: adding implementation for purchase Screen

--- BuildTinder/MatchesView/MatchesView.swift
+++ BuildTinder/MatchesView/MatchesView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct MatchesView: View {
+    
     @EnvironmentObject var userMng: UserManager
+    @EnvironmentObject var appState: AppStateManager
+    
     @State private var selectedTab: LikedTab = .likes
     
     
@@ -16,41 +19,68 @@ struct MatchesView: View {
         case likes
         case topPicks
     }
+    
+    private var buttonText: String {
+        if selectedTab == .likes {
+            return "SEE WHO LIKES YOU"
+        } else {
+            return "UNLOCK TOP PICKS"
+        }
+    }
+    
+    private func buttonAction(){
+        appState.showPurchaseScreen()
+    }
+    
     var body: some View {
-        VStack( spacing: 0){
-            HStack{
-                Spacer()
-                Button(action: { selectedTab = .likes}, label: {
-                    Text("\(userMng.matches.count) likes")
-                        .font(.system(size: 22, weight: .semibold))
-                        .if( selectedTab == .topPicks) {
-                            $0.foregroundColor(.textPrimary)
-                        }
-                })
-                .buttonStyle(PlainButtonStyle())
-            
-                Spacer()
+        ZStack(alignment: .bottom) {
+            VStack( spacing: 0){
+                HStack{
+                    Spacer()
+                    Button(action: { selectedTab = .likes}, label: {
+                        Text("\(userMng.matches.count) likes")
+                            .font(.system(size: 22, weight: .semibold))
+                            .if( selectedTab == .topPicks) {
+                                $0.foregroundColor(.textPrimary)
+                            }
+                    })
+                    .buttonStyle(PlainButtonStyle())
                 
-                Button(action: { selectedTab = .topPicks}, label: {
-                    Text("\(userMng.topPicks.count) Top Picks")
-                        .font(.system(size: 22, weight: .semibold))
-                        .if( selectedTab == .likes) {
-                            $0.foregroundColor(.textPrimary)
-                        }
-                })
-                .buttonStyle(PlainButtonStyle())
+                    Spacer()
+                    
+                    Button(action: { selectedTab = .topPicks}, label: {
+                        Text("\(userMng.topPicks.count) Top Picks")
+                            .font(.system(size: 22, weight: .semibold))
+                            .if( selectedTab == .likes) {
+                                $0.foregroundColor(.textPrimary)
+                            }
+                    })
+                    .buttonStyle(PlainButtonStyle())
+                    Spacer()
+                }
+                Divider()
+                    .padding(.vertical, 14)
+                
+                if selectedTab == .likes {
+                    LikesView()
+                } else {
+                    TopPicksView()
+                }
+                
                 Spacer()
             }
-            Divider()
-                .padding(.vertical, 14)
             
-            if selectedTab == .likes {
-                LikesView()
-            } else {
-                TopPicksView()
-            }
-            
-            Spacer()
+            Button(action: {}, label: {
+                Text(buttonText)
+                    .padding(.vertical, 14)
+                    .padding(.horizontal, 36)
+                    .font(.system(size: 22, weight: .bold))
+                    .foregroundColor(.white)
+                    .background(Color.yellow)
+                    .cornerRadius(30)
+            })
+            .buttonStyle(PlainButtonStyle())
+            .padding(.bottom, 40)
         }
     }
 }
@@ -59,5 +89,6 @@ struct MatchesView_Previews: PreviewProvider {
     static var previews: some View {
         MatchesView()
             .environmentObject(UserManager())
+            .environmentObject(AppStateManager())
     }
 }
