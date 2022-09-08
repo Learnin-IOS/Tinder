443bc2549014864021ae162d6b7b87c901b54040
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-08 22:45:10 +0300

    build: adding age, Job Title and Pro tip to User struct

--- BuildTinder/Model/User.swift
+++ BuildTinder/Model/User.swift
@@ -9,11 +9,21 @@ import Foundation
 
 struct User {
     var name: String
-    
+    var age: Int
+    var jobTitle: String
     var goldSubscriber: Bool = false
+    var profileTip: String =  ""
+    var imageURLS : [URL] = []
+    
 }
 
 
 extension User {
-    static let example = User(name: "Alex", goldSubscriber: false )
+    static let example = User(
+        name: "Le Bon",
+        age: 25,
+        jobTitle: "Software Engineer",
+        goldSubscriber: false, profileTip: "Photo Tip: Makes waves with a beach photo and get more likes" ,
+        imageURLS: [URL(string: "https://picsum.photos/400/310")!]
+    )
 }
