9a3842ebafb41b25db4bc1b2e042f95be48e9eed
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-08 22:46:10 +0300

    fix: adding environment object for app state to collaborate with app state manager

--- BuildTinder/MainTabBar/MainView.swift
+++ BuildTinder/MainTabBar/MainView.swift
@@ -20,7 +20,7 @@ struct MainView: View {
             let view = Text("Fire")
             return AnyView(view)
         case .star:
-            let view = Text("Star")
+            let view = MatchesView()
             return AnyView(view)
         case .message:
             let view = MessageListView()
@@ -72,6 +72,8 @@ struct MainView: View {
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView().environmentObject(AppStateManager())
+        MainView()
+            .environmentObject(AppStateManager())
+            .environmentObject(UserManager())
     }
 }
