5dd843f96cf798fe12ca264e5e257d8ba4c993bd
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-08 20:07:20 +0300

    feat: Implementing a way for users to get more top picks by subscribing to Gold

--- BuildTinder/MatchesView/TopPicksView.swift
+++ BuildTinder/MatchesView/TopPicksView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct TopPicksView: View {
+    @EnvironmentObject var userMng: UserManager
+    @EnvironmentObject var appState: AppStateManager
+    
+    private var user: User {
+        return userMng.currentUser
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false, content: {
+            
+            if !user.goldSubscriber {
+            Text("Upgrade to tinder Gold for more top picks.")
+                .multilineTextAlignment(.center)
+                .lineLimit(2)
+                .foregroundColor(.textTitle)
+                .font(.system(size: 16, weight: .medium))
+                .padding(.horizontal, 40)
+                .padding(.vertical, 24)
+            }
+            
+            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())],
+                      alignment: .center,
+                      spacing: nil,
+                      pinnedViews: [],
+                      content: {
+                ForEach(userMng.matches) { match in
+                    PersonSquare(person: match, blur: !user.goldSubscriber)
+                        .frame(height: 240)
+                        .onTapGesture {
+                            personTapped(match)
+                        }
+                }
+            })
+            .padding(.horizontal, 6)
+        })
+    }
+    
+    func personTapped(_ person: Person){
+        if user.goldSubscriber {
+            appState.showPersonsProfile(person)
+        } else {
+            appState.showPurchaseScreen()
+        }
     }
 }
 
 struct TopPicksView_Previews: PreviewProvider {
     static var previews: some View {
         TopPicksView()
+            .environmentObject(UserManager())
+            .environmentObject(AppStateManager())
     }
 }
