e8a6505bfd23459221e53a8f4989d79b5b36b26f
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-12 21:55:51 +0300

    build: Purchase Options View

---
+++ BuildTinder/Popups/Purchase/PruchaseOptionsView.swift
@@ -0,0 +1,20 @@
+//
+//  PruchaseOptionsView.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 12/09/2022.
+//
+
+import SwiftUI
+
+struct PruchaseOptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PruchaseOptionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PruchaseOptionsView()
+    }
+}
