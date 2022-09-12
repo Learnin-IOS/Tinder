30cb8eadf7e948fa75d199c13328306ca0f44c4f
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-12 11:56:04 +0300

    build: Purchase Swipe Promo

---
+++ BuildTinder/Popups/Purchase/PurchaseSwipePromo.swift
@@ -0,0 +1,20 @@
+//
+//  PurchaseSwipePromo.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 12/09/2022.
+//
+
+import SwiftUI
+
+struct PurchaseSwipePromo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PurchaseSwipePromo_Previews: PreviewProvider {
+    static var previews: some View {
+        PurchaseSwipePromo()
+    }
+}
