b2983669c38d318495a6dd02b81d2e37dce7332b
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-12 21:51:50 +0300

    feat: Implementing Purchase Swipe promo

--- BuildTinder/Popups/Purchase/PurchaseSwipePromo.swift
+++ BuildTinder/Popups/Purchase/PurchaseSwipePromo.swift
@@ -9,12 +9,49 @@ import SwiftUI
 
 struct PurchaseSwipePromo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            VStack(spacing: 10){
+                Image(systemName: "thermometer.sun.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 75)
+                Text("25 people already like !")
+                    .font(.system(size: 18, weight: .bold))
+                
+                Text("Match with them instantly")
+            }
+            VStack(spacing: 10){
+                Image(systemName: "paperplane.circle.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 75)
+                Text("25 people already like !")
+                    .font(.system(size: 18, weight: .bold))
+                
+                Text("Match with them instantly")
+            }
+            VStack(spacing: 10){
+                Image(systemName: "calendar.badge.plus")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 75)
+                Text("25 people already like !")
+                    .font(.system(size: 18, weight: .bold))
+                
+                Text("Match with them instantly")
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
 }
 
 struct PurchaseSwipePromo_Previews: PreviewProvider {
     static var previews: some View {
         PurchaseSwipePromo()
+            .frame(height: UIScreen.main.bounds.height / 3)
     }
 }
