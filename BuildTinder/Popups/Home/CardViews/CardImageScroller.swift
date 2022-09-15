8d8335dddc12b215d824979a9da149b437e984db
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-15 13:40:06 +0300

    build: Card Image Scroller

---
+++ BuildTinder/Popups/Home/CardViews/CardImageScroller.swift
@@ -0,0 +1,20 @@
+//
+//  CardImageScroller.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 15/09/2022.
+//
+
+import SwiftUI
+
+struct CardImageScroller: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardImageScroller_Previews: PreviewProvider {
+    static var previews: some View {
+        CardImageScroller()
+    }
+}
