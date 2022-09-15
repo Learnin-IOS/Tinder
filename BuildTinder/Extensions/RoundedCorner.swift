d057ad2225c20fff67bd4df45dc23916f4e91a87
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-15 12:12:58 +0300

    feat: Support for independently changing the the radius of a specific corner

--- BuildTinder/Extensions/RoundedCorner.swift
+++ BuildTinder/Extensions/RoundedCorner.swift
@@ -5,4 +5,27 @@
 //  Created by Le Bon B' Bauma on 14/09/2022.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct RoundedCorner: Shape {
+    
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+
+extension View {
+    /**
+      Independently changing the radius according to a specific corner
+     */
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
