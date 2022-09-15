077d4e291c2f56ae5c765ce40537982b489e3be1
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-15 13:19:19 +0300

    feat: linking purchase popup to the main view

--- BuildTinder/MainTabBar/MainView.swift
+++ BuildTinder/MainTabBar/MainView.swift
@@ -63,6 +63,12 @@ struct MainView: View {
                     Spacer()
                 }
                 .edgesIgnoringSafeArea(.vertical)
+                
+                if appState.showPurchasePopup {
+                    PurchasePopup(isVisible:  $appState.showPurchasePopup)
+                        .animation(.spring(response: 0.3, dampingFraction: 0.75, blendDuration: 0.5))
+                        .transition(.offset(y: 800))
+                }
             }
             .modifier(HideNavigationView())
         }
