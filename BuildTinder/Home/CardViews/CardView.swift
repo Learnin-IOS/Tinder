d2ba6bcd775eccd3254a4e7310d5ead7b5f634ad
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-15 15:37:40 +0300

    build: Card View

---
+++ BuildTinder/Home/CardViews/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 15/09/2022.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
