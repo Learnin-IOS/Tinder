ca6a79c5dba2bed163bf788ba573b9d460028c7b
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-15 15:22:58 +0300

    feat: Implementing Image Scroller : Selected Index Indicator

--- BuildTinder/Popups/Home/CardViews/CardImageScroller.swift
+++ BuildTinder/Popups/Home/CardViews/CardImageScroller.swift
@@ -15,6 +15,8 @@ struct CardImageScroller: View {
     
     @State private var imageIndex = 0
     
+    @Binding var fullScreenMode : Bool
+    
     func updateImageIndex(addition: Bool){
         let newIndex: Int
         
@@ -28,35 +30,80 @@ struct CardImageScroller: View {
     }
     var body: some View {
         GeometryReader { geo in
-            ZStack{
-                KFImage(person.imagesURLS[imageIndex])
-                    .placeholder {
-                        Color.white
-                    }
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: geo.size.width, height: geo.size.height)
-                    .clipped()
-                
-                HStack{
-                    Rectangle()
-                        .onTapGesture {
-                            updateImageIndex(addition: false )
+            ZStack {
+                ZStack{
+                    KFImage(person.imagesURLS[imageIndex])
+                        .placeholder {
+                            Color.white
                         }
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: geo.size.width, height: geo.size.height)
+                        .clipped()
                     
-                    Rectangle()
-                        .onTapGesture {
-                            updateImageIndex(addition: true)
-                        }
+                    HStack{
+                        Rectangle()
+                            .onTapGesture {
+                                updateImageIndex(addition: false )
+                            }
+                        
+                        Rectangle()
+                            .onTapGesture {
+                                updateImageIndex(addition: true)
+                            }
+                    }
+                    .foregroundColor(Color.white.opacity(0.001))
                 }
-                .foregroundColor(Color.white.opacity(0.001))
+                // Building indicator bars
+                VStack{
+                    HStack{
+                        ForEach(0..<person.imagesURLS.count) { imageIndex in
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(height: 4)
+                                .foregroundColor(self.imageIndex == imageIndex ? Color.white : Color.gray.opacity(0.5))
+                        }
+                    }
+                    .padding(.top, 6)
+                    .padding(.horizontal, fullScreenMode ? 0 : 12)
+                    
+                    Spacer()
+                    
+                    if !fullScreenMode {
+                        HStack{
+                            VStack(alignment: .leading){
+                                HStack{
+                                    Text(person.name)
+                                        .font(.system(size: 32,weight: .heavy))
+                                    
+                                    Text(String(person.age))
+                                        .font(.system(size: 28, weight: .light))
+                                }
+                                
+                                Text(person.bio)
+                                    .font(.system(size: 18, weight: .medium))
+                                    .lineLimit(2)
+                            }
+                            Spacer()
+                            
+                            Button(action: { fullScreenMode = true }, label: {
+                                Image(systemName: "info.circle.fill")
+                                    .font(.system(size: 26, weight: .medium))
+                                    
+                            })
+                        }
+                        .padding(16)
+                    }
+                }
+                .foregroundColor(Color.white)
             }
+            .cornerRadius(6)
+            .shadow(radius: 5 )
         }
     }
-}
-
-struct CardImageScroller_Previews: PreviewProvider {
-    static var previews: some View {
-        CardImageScroller(person: Person.example)
+    
+    struct CardImageScroller_Previews: PreviewProvider {
+        static var previews: some View {
+            CardImageScroller(person: Person.example, fullScreenMode: .constant(true))
+        }
     }
 }
