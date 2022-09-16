030380213888f8e0428801d6b1fc93cd94678f5d
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-16 05:55:20 +0300

    build: Updating UserManger for placeholders for networking

--- BuildTinder/UserManager.swift
+++ BuildTinder/UserManager.swift
@@ -11,11 +11,17 @@ class UserManager: ObservableObject {
     @Published var currentUser: User = User(name: "", age: 0, jobTitle: "b")
     @Published var matches: [Person] = []
     @Published var topPicks: [Person] = []
+    @Published var cardPeople: [Person] = []
     
     init(){
         loadUser()
         loadMatches()
         loadTopPicks()
+        loadCardPeople()
+    }
+    
+    private func loadCardPeople(){
+        self.cardPeople = Person.examples
     }
     
    private func loadUser(){
@@ -28,4 +34,18 @@ class UserManager: ObservableObject {
     private func loadTopPicks(){
         self.topPicks = Person.examples.shuffled()
     }
+    
+    public func swipe(_ person: Person, _ direction: SwipeDirection){
+        //Network Call to backend
+        cardPeople.removeLast()
+    }
+    public func superLike(_ person: Person) {
+        //Network Call to backend 
+        cardPeople.removeLast()
+    }
+}
+
+enum SwipeDirection {
+    case like
+    case nope
 }
