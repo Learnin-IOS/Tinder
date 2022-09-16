b4d4c5b5f16ffa607bf25122bb0fd53f927bedc2
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-16 04:39:06 +0300

    feat: Implementing Buttons for Helper  function

--- BuildTinder/HelperView/CircleButtonView.swift
+++ BuildTinder/HelperView/CircleButtonView.swift
@@ -6,15 +6,49 @@
 //
 
 import SwiftUI
+enum ButtonType : String {
+    case back = "gobackward"
+    case no = "xmark"
+    case star = "star.fill"
+    case heart = "heart.fill"
+    case lightning = "cloud.bolt.fill"
+}
 
 struct CircleButtonView: View {
+    var type: ButtonType
+    
+    var action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: { action() }, label: {
+            Image(systemName: type.rawValue)
+                .resizable()
+                .font(.system(size: 12, weight: .bold))
+                .aspectRatio(contentMode: .fit)
+        })
     }
 }
 
 struct CircleButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        CircleButtonView()
+        
+        ZStack{
+            Color.gray.opacity(0.2)
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 50 ){
+                CircleButtonView(type: .back) {}
+                    .frame(height: 50)
+                CircleButtonView(type: .no) {}
+                    .frame(height: 50)
+                CircleButtonView(type: .star) {}
+                    .frame(height: 50)
+                CircleButtonView(type: .heart) {}
+                    .frame(height: 50)
+                CircleButtonView(type: .lightning) {}
+                    .frame(height: 50)
+            }
+            
+        }
+       
     }
 }
