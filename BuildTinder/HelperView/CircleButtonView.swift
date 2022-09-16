eed404435e0625f7ec62b73a63cef3c6bfc9d8fd
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-16 04:17:48 +0300

    build: CircleButtonView

---
+++ BuildTinder/HelperView/CircleButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleButtonView.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 16/09/2022.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleButtonView()
+    }
+}
