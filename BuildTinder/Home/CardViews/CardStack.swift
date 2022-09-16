a94fec140810b8e14f96283c681fd106f4f4cdbf
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-16 07:25:26 +0300

    build: CardStack

---
+++ BuildTinder/Home/CardViews/CardStack.swift
@@ -0,0 +1,20 @@
+//
+//  CardStack.swift
+//  BuildTinder
+//
+//  Created by Le Bon B' Bauma on 16/09/2022.
+//
+
+import SwiftUI
+
+struct CardStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardStack_Previews: PreviewProvider {
+    static var previews: some View {
+        CardStack()
+    }
+}
