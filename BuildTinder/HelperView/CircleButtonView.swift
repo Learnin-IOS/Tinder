e05a2f4619c6abc8544a137f61aa0a3b97381e4e
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-16 05:03:42 +0300

    style: Implementing ButtonStyle and  independent Color View Modifiers on the buttons

--- BuildTinder/HelperView/CircleButtonView.swift
+++ BuildTinder/HelperView/CircleButtonView.swift
@@ -14,6 +14,47 @@ enum ButtonType : String {
     case lightning = "cloud.bolt.fill"
 }
 
+struct CircleButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .background(
+                Circle()
+                    .foregroundColor(Color.white)
+            )
+            .scaleEffect(configuration.isPressed ? 0.7 : 1.0)
+    }
+}
+
+
+struct ColorButton: ViewModifier {
+    var type: ButtonType
+    
+    func body(content: Content) -> some View {
+        switch type {
+        case .back:
+            return AnyView(
+                content
+                 .foregroundColor(.yellow))
+        case .no:
+            return AnyView(
+                content
+                    .foregroundColor(.red))
+        case .star:
+            return AnyView(
+                content
+                    .foregroundColor(.blue))
+        case .heart:
+            return AnyView(
+            content
+                .foregroundColor(.green))
+        case .lightning:
+            return AnyView(
+            content
+                .foregroundColor(.purple))
+        }
+    }
+}
+
 struct CircleButtonView: View {
     var type: ButtonType
     
@@ -25,9 +66,14 @@ struct CircleButtonView: View {
                 .resizable()
                 .font(.system(size: 12, weight: .bold))
                 .aspectRatio(contentMode: .fit)
+                .padding(12)
         })
+        .buttonStyle(CircleButton())
+        .modifier(ColorButton(type: type))
     }
 }
+
+
 
 struct CircleButtonView_Previews: PreviewProvider {
     static var previews: some View {
