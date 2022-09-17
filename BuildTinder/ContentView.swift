2d11d56d7a5e62b3a008986d8c376e801ab57429
Le Bon Bauma <lebonbauma.dev@gmail.com>
2022-09-17 03:19:45 +0300

    refactor: ContentView

--- BuildTinder/ContentView.swift
+++ BuildTinder/ContentView.swift
@@ -10,13 +10,16 @@ import SwiftUI
 struct ContentView: View {
     
     @ObservedObject var manager: AppStateManager = AppStateManager()
+    
     @ObservedObject var userMng: UserManager = UserManager()
+    
     var body: some View {
         MainView()
             .environmentObject(manager)
             .environmentObject(userMng)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
